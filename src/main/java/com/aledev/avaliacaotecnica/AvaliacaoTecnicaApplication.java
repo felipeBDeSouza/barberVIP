2c733d4ed02c7d02c4bbd478e1f1f650ae912083
Alessandro <alessandro.souza@edu.pucrs.br>
2024-06-29 13:54:56 -0300

    teste

--- src/main/java/com/aledev/avaliacaotecnica/AvaliacaoTecnicaApplication.java
+++ src/main/java/com/aledev/avaliacaotecnica/AvaliacaoTecnicaApplication.java
@@ -12,5 +12,6 @@ public class AvaliacaoTecnicaApplication {
     public static void main(String[] args) {
         SpringApplication.run(AvaliacaoTecnicaApplication.class, args);
     }
+    //teste branch
 
 }
